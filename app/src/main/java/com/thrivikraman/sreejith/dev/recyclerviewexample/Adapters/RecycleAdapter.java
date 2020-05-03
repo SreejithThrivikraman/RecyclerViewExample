3c4b5b0c544005876731a1d7dd04e7358bba6d94
sree <mail.sreejith.23@gmail.com>
2020-05-03 19:14:22 -0400

    "Completed Recycler adapter. Need to fix bugs"

--- app/src/main/java/com/thrivikraman/sreejith/dev/recyclerviewexample/Adapters/RecycleAdapter.java
+++ app/src/main/java/com/thrivikraman/sreejith/dev/recyclerviewexample/Adapters/RecycleAdapter.java
@@ -1,8 +1,10 @@
 package com.thrivikraman.sreejith.dev.recyclerviewexample.Adapters;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -10,22 +12,44 @@ import android.widget.TextView;
 
 import com.thrivikraman.sreejith.dev.recyclerviewexample.R;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.LocationViewHolder>  {
+
+
+    private ArrayList<String> locationName = new ArrayList<>();
+    private ArrayList<String> imageName = new ArrayList<>();
+    private Context mContext;
+
+    public RecycleAdapter(Context context, ArrayList<String> imageNames, ArrayList<String> locationNames)
+    {
+        locationName = locationNames;
+        mContext = context;
+
+    }
+
 
     @NonNull
     @Override
-    public LocationViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+    public LocationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.itemlayout,parent,false);
+        LocationViewHolder viewHolder = new LocationViewHolder(view);
+
+        return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull LocationViewHolder locationViewHolder, int i) {
+    public void onBindViewHolder(@NonNull LocationViewHolder locationViewHolder, int position) {
+
+        locationViewHolder.LocationName.setText(locationName.get(position));
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return imageName.size();
     }
 
     public static class LocationViewHolder extends RecyclerView.ViewHolder
--- app/src/main/java/com/thrivikraman/sreejith/dev/recyclerviewexample/MainActivity.java
+++ app/src/main/java/com/thrivikraman/sreejith/dev/recyclerviewexample/MainActivity.java
@@ -2,11 +2,20 @@ package com.thrivikraman.sreejith.dev.recyclerviewexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
+import com.thrivikraman.sreejith.dev.recyclerviewexample.Adapters.RecycleAdapter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView R_List;
+
+    protected ArrayList<String> locationNames = new ArrayList<>();
+    protected ArrayList<String> imageNames = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +25,15 @@ public class MainActivity extends AppCompatActivity {
         R_List = findViewById(R.id.recycle_list);
 
         R_List.setHasFixedSize(true);
+
+
+        // initialize arraylist
+        locationNames = new ArrayList<String>(  Arrays.asList(getResources().getStringArray(R.array.locations)));
+
+        RecycleAdapter recycleAdapter = new RecycleAdapter(getApplicationContext(),imageNames,locationNames);
+        R_List.setAdapter(recycleAdapter);
+        R_List.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
     }
 
 
